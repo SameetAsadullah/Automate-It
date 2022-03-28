20ea9cf1d1b2b0d8ca3892b3c1091ff964b557f0
ayshanoorcode <aysha313noor@gmail.com>
2022-03-28 14:23:00 +0500

    updated configuration for mac

---
+++ src/navigation/AppStack.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LogIn from '../screens/LogIn';
+import SignUp from '../screens/SignUp';
+import LoadScreen from '../screens/LoadScreen';
+import HomePage from "../screens/HomePage"
+import MicrophoneInput from '../screens/MicrophoneInput';
+import YourAppliance from '../screens/YourAppliance';
+import AcRemote from '../screens/AcRemote';
+import TvRemote from '../screens/TvRemote';
+import MusicControls from '../screens/MusicControls';
+import AddAppliance from '../screens/AddAppliance';
+import AcModels from '../screens/AcModels';
+
+const Stack = createStackNavigator();
+
+export default function AppStack() {
+  return (
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+      >
+        <Stack.Screen name="Loading" component={LoadScreen} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="MicrophoneInput" component={MicrophoneInput} />
+        <Stack.Screen name="YourAppliance" component={YourAppliance} />
+        <Stack.Screen name="AcRemote" component={AcRemote} />
+        <Stack.Screen name="TvRemote" component={TvRemote} />
+        <Stack.Screen name="MusicControls" component={MusicControls} />
+        <Stack.Screen name="AddAppliance" component={AddAppliance} />
+        <Stack.Screen name="AcModels" component={AcModels} />
+      </Stack.Navigator>
+  );
+}
