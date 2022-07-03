b1fe2b09aa0ccbc26ef4651f36054205a519fd10
ayshanoorcode <aysha313noor@gmail.com>
2022-07-03 23:10:13 +0500

    ignore warnings

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ import AppStack from './src/navigation/AppStack'
 import { Provider } from 'react-redux';
 import store from './src/user_files/data/store'
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
+
 const Stack = createStackNavigator();
 
 function MyStack() {
